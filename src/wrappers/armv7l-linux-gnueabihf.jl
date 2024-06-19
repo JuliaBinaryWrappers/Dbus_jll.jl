d3ffcd36827172fb0667a8e5653607b410f6ad5f
jlbuild <juliabuildbot@gmail.com>
2024-06-19 10:22:32 +0000

    Dbus_jll build 1.14.10+0

--- src/wrappers/armv7l-linux-gnueabihf.jl
+++ src/wrappers/armv7l-linux-gnueabihf.jl
@@ -3,19 +3,19 @@ export dbus_daemon, libdbus
 
 using Expat_jll
 JLLWrappers.@generate_wrapper_header("Dbus")
-JLLWrappers.@declare_executable_product(dbus_daemon)
 JLLWrappers.@declare_library_product(libdbus, "libdbus-1.so.3")
+JLLWrappers.@declare_executable_product(dbus_daemon)
 function __init__()
     JLLWrappers.@generate_init_header(Expat_jll)
-    JLLWrappers.@init_executable_product(
-        dbus_daemon,
-        "bin/dbus-daemon",
-    )
-
     JLLWrappers.@init_library_product(
         libdbus,
         "lib/libdbus-1.so",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        dbus_daemon,
+        "bin/dbus-daemon",
     )
 
     JLLWrappers.@generate_init_footer()
